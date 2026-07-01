941b7854aba0276b88877911ed6e30d632414629
sergiobancestorree-creator <sergiobancestorree@gmail.com>
2026-07-01 01:51:57 -0500

    Update products-data.js

--- js/products-data.js
+++ js/products-data.js
@@ -13,7 +13,11 @@ const products = [
     id: 1,
     nombre: "Carbón Activado 100g",
     categoria: "Jabón",
-    beneficios: ["x", "y", "z"],
+    beneficios: [
+      "Limpieza y purificación de piel grasosa y con acné", 
+      "Mejora de piel sometida a contaminación, estrés e infecciones", 
+      "Absorbente de bacterias"
+    ],
     precio: 10.00,
     foto: "./assets/jabon-carbon-activado.jpeg"
   },
@@ -31,7 +35,12 @@ const products = [
     id: 3,
     nombre: "Concha de nacar, arcilla blanca y plátano 100g",
     categoria: "Jabón",
-    beneficios: ["x", "y", "z"],
+    beneficios: [
+      "Regeneración de daños por el acné", 
+      "Prevención del acné", 
+      "Eliminación de manchas", 
+      "Limpieza de cutis grasos"
+    ],
     precio: 10.00,
     foto: "./assets/jabon-nacar-arcilla-platano.jpeg"
   },
@@ -40,7 +49,11 @@ const products = [
     id: 4,
     nombre: "Cúrcuma y aceite de coco 100g",
     categoria: "Jabón",
-    beneficios: ["x", "y", "z"],
+    beneficios: [
+      "Reducción de pigmentaciones oscuras", 
+      "Antioxidante y exfoliante natural", 
+      "Ideal para piel seca, delicada y/o enrojecida"
+    ],
     precio: 10.00,
     foto: "./assets/jabon-curcuma-aceite-coco.jpeg"
   },
@@ -49,7 +62,11 @@ const products = [
     id: 5,
     nombre: "Coco y aceite de castañas 100g",
     categoria: "Jabón",
-    beneficios: ["x", "y", "z"],
+    beneficios: ["Suavizante, hidratante, nutritivo", 
+                 "Rico en ácido láurico antioxidante", 
+                 "Astringente, antiinflamatorio", 
+                 "Ayuda a piel sensible y enrojecida"
+                ],
     precio: 10.00,
     foto: "./assets/jabon-coco-aceite-castañas.jpeg"
   },
@@ -58,7 +75,11 @@ const products = [
     id: 6,
     nombre: "Kion , anís y manzanilla 100g",
     categoria: "Jabón",
-    beneficios: ["x", "y", "z"],
+    beneficios: [
+      "Ideal para piel delicada y dermatitis", 
+      "Hidratante y humectante", 
+      "Antiinflamatorio"
+    ],
     precio: 10.00,
     foto: "./assets/jabon-kion-anís-manzanilla.jpeg"
   },
@@ -94,7 +115,11 @@ const products = [
     id: 10,
     nombre: "Rosas 100g",
     categoria: "Jabón",
-    beneficios: ["x", "y", "z"],
+    beneficios: [
+      "Hidratante, suavizador, antioxidante, regenerador", 
+      "Aporta a la elasticidad de la piel", 
+      "Ideal para piel cansada"
+    ],
     precio: 10.00,
     foto: "./assets/jabon-rosas.jpeg"
   },
@@ -103,7 +128,11 @@ const products = [
     id: 11,
     nombre: "Lavanda 100g",
     categoria: "Jabón",
-    beneficios: ["x", "y", "z"],
+    beneficios: [
+      "Tratamiento de inflamación, quemaduras y psoriasis", 
+      "Antiséptico y regenerador", 
+      "Prevención del acné"
+    ],
     precio: 10.00,
     foto: "./assets/jabon-lavanda.jpeg"
   },
@@ -112,7 +141,11 @@ const products = [
     id: 12,
     nombre: "Avena, miel y aceite de castañas 100g",
     categoria: "Jabón",
-    beneficios: ["x", "y", "z"],
+    beneficios: [
+      "Suavizante, exfoliante, humectante, hidratante", 
+      "Cicatrizante, antiinflamatorio, antibacteriano", 
+      "Regulador de piel grasosa"
+    ],
     precio: 10.00,
     foto: "./assets/jabon-avena-miel-castañas.jpeg"
   },
@@ -130,7 +163,11 @@ const products = [
     id: 14,
     nombre: "Canela 100g",
     categoria: "Jabón",
-    beneficios: ["x", "y", "z"],
+    beneficios: [
+      "Estimulante de las funciones circulatorias y respiratorias", 
+      "Bactericida, antiséptico, calmante de dolor", 
+      "Antioxidante y prvención del acné y espinillas"
+    ],
     precio: 10.00,
     foto: "./assets/jabon-canela.jpeg"
   },
@@ -148,7 +185,10 @@ const products = [
     id: 16,
     nombre: "Shampoo de Cúrcuma, Quinoa, Romero 60g",
     categoria: "Sólido",
-    beneficios: ["x", "y", "z"],
+    beneficios: [
+      "Acondicionador 2 en 1", 
+      "Adecuado para todo tipo de cabello"
+    ],
     precio: 30.00,
     foto: "./assets/solido-cúrcuma-quinoa-romero.jpeg"
   },
@@ -157,7 +197,10 @@ const products = [
     id: 17,
     nombre: "Acondicionador de Manteca de cacao y romero 60g",
     categoria: "Sólido",
-    beneficios: ["x", "y", "z"],
+    beneficios: [
+      "Hidratación y suavizantes naturales", 
+      "Ideal para desenredar el cabello"
+    ],
     precio: 30.00,
     foto: "./assets/solido-manteca-cacao-romero.jpeg"
   },
@@ -166,7 +209,9 @@ const products = [
     id: 18,
     nombre: "Cotton Baby Care",
     categoria: "Aromatizador",
-    beneficios: [],
+    beneficios: [
+      "Fragancia de larga duración. Refresca cualquier espacio al instante y brinda sensación de limpieza. Ideal para usar en ropa de cama, cortinas, sofás, y habitaciones"
+    ],
     precio: 50.00,
     foto: "./assets/aromatizador-cotton-baby-care.jpeg"
   },
@@ -175,7 +220,9 @@ const products = [
     id: 19,
     nombre: "Elegance - Sofisticado",
     categoria: "Aromatizador",
-    beneficios: [],
+    beneficios: [
+      "Fragancia de larga duración. Refresca cualquier espacio al instante y brinda sensación de limpieza. Ideal para usar en ropa de cama, cortinas, sofás, y habitaciones"
+    ],
     precio: 50.00,
     foto: "./assets/aromatizador-elegance-sofisticado.jpeg"
   },
@@ -184,7 +231,9 @@ const products = [
     id: 20,
     nombre: "Manzana, canela y vainilla",
     categoria: "Aromatizador",
-    beneficios: [],
+    beneficios: [
+      "Fragancia de larga duración. Refresca cualquier espacio al instante y brinda sensación de limpieza. Ideal para usar en ropa de cama, cortinas, sofás, y habitaciones"
+    ],
     precio: 50.00,
     foto: "./assets/aromatizador-manzana-canela-vainilla.jpeg"
   },
@@ -193,7 +242,9 @@ const products = [
     id: 21,
     nombre: "Lavanda con manzanilla",
     categoria: "Aromatizador",
-    beneficios: [],
+    beneficios: [
+      "Fragancia de larga duración. Refresca cualquier espacio al instante y brinda sensación de limpieza. Ideal para usar en ropa de cama, cortinas, sofás, y habitaciones"
+      ],
     precio: 50.00,
     foto: "./assets/aromatizador-lavanda-manzanilla.jpeg"
   },
@@ -202,7 +253,9 @@ const products = [
     id: 22,
     nombre: "Sweet floral",
     categoria: "Aromatizador",
-    beneficios: [],
+    beneficios: [
+      "Fragancia de larga duración. Refresca cualquier espacio al instante y brinda sensación de limpieza. Ideal para usar en ropa de cama, cortinas, sofás, y habitaciones"
+    ],
     precio: 50.00,
     foto: "./assets/aromatizador-sweet-floral.jpeg"
   },
@@ -211,7 +264,9 @@ const products = [
     id: 23,
     nombre: "Coco, lima y verbena",
     categoria: "Aromatizador",
-    beneficios: [],
+    beneficios: [
+      "Fragancia de larga duración. Refresca cualquier espacio al instante y brinda sensación de limpieza. Ideal para usar en ropa de cama, cortinas, sofás, y habitaciones"
+    ],
     precio: 50.00,
     foto: "./assets/aromatizador-coco-lima-verbena.jpeg"
   },
@@ -220,7 +275,9 @@ const products = [
     id: 24,
     nombre: "Caja de trupam con 4 jabones 100g + bolsa de tocuyo",
     categoria: "Box",
-    beneficios: ["Surtido"],
+    beneficios: [
+      "Surtido"
+    ],
     precio: 55.00,
     foto: "./assets/box-4-jabones.jpeg"
   },
@@ -229,7 +286,9 @@ const products = [
     id: 25,
     nombre: "Caja de trupam con 3 jabones 100g + bolsa de tocuyo",
     categoria: "Box",
-    beneficios: ["Surtido"],
+    beneficios: [
+      "Surtido"
+    ],
     precio: 50.00,
     foto: "./assets/box-caja-3-jabones.jpeg"
   },
@@ -238,7 +297,9 @@ const products = [
     id: 26,
     nombre: "Caja de trupam con 9 mini jabones 23g",
     categoria: "Box",
-    beneficios: ["Surtido"],
+    beneficios: [
+      "Surtido"
+    ],
     precio: 40.00,
     foto: "./assets/box-caja-9-minis.jpeg"
   },
@@ -247,7 +308,9 @@ const products = [
     id: 27,
     nombre: "Caja de trupam con 9 medios jabones 45g",
     categoria: "Box",
-    beneficios: ["Surtido"],
+    beneficios: [
+      "Surtido"
+    ],
     precio: 70.00,
     foto: "./assets/box-caja-9-medios.jpeg"
   },
@@ -334,7 +397,7 @@ const products = [
 
   {
     id: 36,
-    nombre: "Desodorante de Alumbre, árbol de té y sándalo",
+    nombre: "Desodorante de Alumbre",
     categoria: "Higiene Personal",
     beneficios: ["x", "y", "z"],
     precio: 25.00,
